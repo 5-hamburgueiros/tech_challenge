abe8d9c0af94fa6171908b42828be281ac03441e
Vinicius Dias <carlosv775@gmail.com>
2024-01-25 18:20:28 -0300

    chore: Ajustando para rodar na AWS (com EKS e RDS) (#22)
    * chore: Usando ssl para se conectar em prod
    
    * chore: Fazendo funcionar na AWS
    
    * chore: Removendo aws-pv.yaml
    
    * chore: Undoing pvc storageClassName
    
    * chore: Corrigindo workflow sem usar buildx
    
    * fix: Corrigindo nome da action
    
    * chore: Adicionando informações do mercado pago e corrigindo swagger
    
    * chore: Quebra de linha

--- src/infra/configs/typeorm.config.ts
+++ src/infra/configs/typeorm.config.ts
@@ -16,7 +16,11 @@ export class TypeORMConfig implements IConfig<TypeOrmModuleOptions> {
       username: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASS,
       database: process.env.DATABASE_NAME,
-      ssl: false,
+      ssl: environment.isDevelopment()
+        ? false
+        : {
+            rejectUnauthorized: false,
+          },
       synchronize: true,
       entities: typeOrmEntities,
       maxQueryExecutionTime: 60000,
